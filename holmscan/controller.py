7cca89146a9870d00b7e21b26f16855d997ee0b0
Henrik Holmboe <henrik@dynamist.se>
2018-10-26 13:25:36 +0200

    Refactor the login logic into the CLI

--- holmscan/controller.py
+++ holmscan/controller.py
@@ -3,17 +3,19 @@
 # python std lib
 import copy
 from distutils.util import strtobool
+import json
 import logging
 import os
 import re
 
 # holmscan imports
 from holmscan.exceptions import HolmscanConfigException
-from holmscan import scan
+from holmscan.scan import Scan
 
 # 3rd party imports
 import anyconfig
 import appdirs
+import requests
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
@@ -55,8 +57,10 @@ class Controller(object):
                 "Loglevel is: {}".format(logging.getLevelName(log.getEffectiveLevel()))
             )
 
+        # Load configuration files and environment variables
         self.conf = self._load_config()
 
+        # Print the resulting/active configuration
         maxlen = 8 + len(max(dict(self.conf).keys(), key=len))
         for k, v in dict(self.conf).items():
             log.debug("{} {} {}".format(k, "." * (maxlen - len(k)), v))
@@ -79,7 +83,11 @@ class Controller(object):
                     error += ", " + example
                 raise HolmscanConfigException(error)
 
-        self.scan = scan.Scan(self.conf)
+        log.debug("Initialized controller for %s", self.conf.get("HOLMSEC_URL"))
+
+        self.loggedin = False
+        self.session = requests.session()
+        self.scan = Scan(self)
 
     def _load_config(self):
         """
@@ -157,3 +165,38 @@ class Controller(object):
         anyconfig.merge(conf, {k: v for k, v in environ.items() if k in CONFIGURABLES})
 
         return conf
+
+    def _login(self):
+        """
+        """
+        url = u"{0}login/in".format(self.conf.get("HOLMSEC_URL"))
+        username = self.conf.get("HOLMSEC_USERNAME")
+        password = self.conf.get("HOLMSEC_PASSWORD")
+        data = json.dumps(
+            {
+                "username": username,
+                "password": password,
+                "redirect": "",
+                "language": "en",
+            }
+        )
+
+        log.debug("Logging in to Security Center at {}".format(url))
+        log.debug("JSON data: {0}".format(data))
+
+        response = self.session.post(url, data=data)
+        log.debug("Response from Security Center: {0}".format(response))
+
+        self.loggedin = True
+
+        return response
+
+    def _logout(self):
+        """
+        """
+        log.debug("Logging out of Security Center")
+        self.loggedin = False
+
+        # TODO logout is not implemented
+        import NotImplementedError
+        raise NotImplementedError
--- holmscan/interface.py
+++ holmscan/interface.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 
+# python std lib
+import logging
+
 # holmscan imports
 from holmscan.exceptions import HolmscanRemoteException
 
+log = logging.getLogger(__name__)
+
 
 class HolmscanModule(object):
-    def __init__(self, configuration):
-        print("HOLMSEC_URL: {}".format(configuration.get("HOLMSEC_URL")))
-        print("HOLMSEC_USERNAME: {}".format(configuration.get("HOLMSEC_USERNAME")))
-        print("HOLMSEC_PASSWORD: {}".format(configuration.get("HOLMSEC_PASSWORD")))
+    def __init__(self, controller):
+        self.controller = controller
