1ec65a0ba2be9a282548b939093232abb6e7159f
Henrik Holmboe <henrik@dynamist.se>
2019-10-11 00:08:54 +0200

    Remove unused imports

--- holmscan/controller.py
+++ holmscan/controller.py
@@ -3,7 +3,6 @@
 # python std lib
 import copy
 from distutils.util import strtobool
-import json
 import logging
 import os
 import re
--- holmscan/interface.py
+++ holmscan/interface.py
@@ -1,13 +1,5 @@
 # -*- coding: utf-8 -*-
 
-# python std lib
-import logging
-
-# holmscan imports
-from holmscan.exceptions import HolmscanRemoteException
-
-log = logging.getLogger(__name__)
-
 
 class HolmscanModule(object):
     def __init__(self, controller):
